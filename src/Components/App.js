3709c71168afe37f66af7fbd64907f545bfe087b
Shanen Cross <shanencross@yahoo.com>
2021-08-24 16:00:46 -0700

    Add Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import CauldronControl from "./CauldronControl";
 
 function App() {
   return (
     <React.Fragment>
+      <Header/>
       <CauldronControl/>
     </React.Fragment>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Potion Shop</h1>
+      <em>Purchase a potion from one of our many cauldrons!</em>
+    </React.Fragment>
+  )
+}
+
+export default Header;
