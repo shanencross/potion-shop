eb2b543bc16fb06480be89ce1e315810f74fb9dc
Shanen Cross <shanencross@yahoo.com>
2021-08-24 16:17:23 -0700

    Use destructuring to clean up look of code

--- src/Components/Cauldron.js
+++ src/Components/Cauldron.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Cauldron(props) {
+  const { name, brand, price } = props;
   return (
     <React.Fragment>
-      <h3>{props.name} - <em>${props.price}</em></h3>
+      <h3>{name} - <em>${price}</em></h3>
       <p>
         Brewed for you by:
         <br/>
-        <em>{props.brand}</em>
+        <em>{brand}</em>
       </p>
       <hr/>
     </React.Fragment>
