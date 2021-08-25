98b01caa8420613667bf7db7152ada29ae75b2a1
Shanen Cross <shanencross@yahoo.com>
2021-08-24 19:28:45 -0700

    Move data to CauldronControl and create/display details page

--- src/Components/CauldronControl.js
+++ src/Components/CauldronControl.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Menu from "./Menu";
+import cauldrons from "../data/cauldrons";
+import CauldronDetails from "./CauldronDetails";
 
 function CauldronControl(props) {
   return (
     <React.Fragment>
-      <Menu/>
+      {/* <Menu menu={cauldrons}/> */}
+      <CauldronDetails cauldron={cauldrons[0]}/>
     </React.Fragment>
   );
 }
--- src/Components/CauldronDetails.js
+++ src/Components/CauldronDetails.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function CauldronDetails(props) {
+  const { cauldron } = props;
+  const  { name, brand, price, description, magicConcentration, pints } = cauldron;
   return (
     <React.Fragment>
-
+      <h1>{name}</h1>
+      <button><strong>Buy pint</strong> for ${price}</button>
+      <p>
+        <strong>Brewed for you by:</strong> <em>{brand}</em>
+        <br/>
+        <strong>Effect: </strong>{description}
+        <br/>
+        <strong>Magic concentration:</strong> {magicConcentration}
+        <br/>
+        <strong>Pints left:</strong> {pints}
+      </p>
     </React.Fragment>
   );
 }
